a769cafeb10336a0e03a9c1d9e36ae4ffbd3e9db
Arnav Agarwal <arnav22agarwal@gmail.com>
2025-04-02 21:01:09 +0530

    safe point with images section parallax for about us
    safe point images parallax for about us added

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,8 +1,95 @@
+"use client";
 import React from 'react'
+import { HeroParallax } from '@/components/blocks/hero-parallax';
 
 function page() {
+
+  const products = [
+    {
+      title: "Moonbeam",
+      link: "https://gomoonbeam.com", 
+      thumbnail: "/Images/about/image1.png",
+    },
+    {
+      title: "Cursor",
+      link: "https://cursor.so",
+      thumbnail: "/Images/about/image2.png",
+    },
+    {
+      title: "Rogue",
+      link: "https://userogue.com",
+      thumbnail: "/Images/about/rogue.png",
+    },
+    {
+      title: "Editorially",
+      link: "https://editorially.org",
+      thumbnail: "/Images/about/editorially.png",
+    },
+    {
+      title: "Editrix AI",
+      link: "https://editrix.ai",
+      thumbnail: "/Images/about/editrix.png",
+    },
+    {
+      title: "Pixel Perfect",
+      link: "https://app.pixelperfect.quest",
+      thumbnail: "/Images/about/pixelperfect.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      thumbnail: "/Images/about/algochurn.png",
+    },
+    {
+      title: "Aceternity UI",
+      link: "https://ui.aceternity.com",
+      thumbnail: "/Images/about/aceternityui.png",
+    },
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      thumbnail: "/Images/about/tailwindmasterkit.png",
+    },
+    {
+      title: "SmartBridge",
+      link: "https://smartbridgetech.com",
+      thumbnail: "/Images/about/smartbridge.png",
+    },
+    {
+      title: "Renderwork Studio",
+      link: "https://renderwork.studio",
+      thumbnail: "/Images/about/renderwork.png",
+    },
+    {
+      title: "Creme Digital",
+      link: "https://cremedigital.com",
+      thumbnail: "/Images/about/cremedigital.png",
+    },
+    {
+      title: "Golden Bells Academy",
+      link: "https://goldenbellsacademy.com",
+      thumbnail: "/Images/about/goldenbellsacademy.png",
+    },
+    {
+      title: "Invoker Labs",
+      link: "https://invoker.lol",
+      thumbnail: "/Images/about/invoker.png",
+    },
+    {
+      title: "E Free Invoice",
+      link: "https://efreeinvoice.com",
+      thumbnail: "/Images/about/efreeinvoice.png",
+    },
+  ];
+  
   return (
-    <div>page</div>
+    <div className="min-h-screen w-full">
+      <div className="absolute top-0 left-0 w-full">
+        <HeroParallax products={products} />
+      </div>
+    </div>
+
+
   )
 }
 
