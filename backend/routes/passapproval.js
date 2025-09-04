60ed51aa885e79240e37b865552434c72b4c9e7e
Arnav Agarwal <137907741+Arnav-Agarwal-01@users.noreply.github.com>
2025-09-04 20:31:40 +0530

    removing backend

--- backend/routes/passapproval.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { ApproveEntry } = require('../controllers/entrycontroller');
-
-
-// Protected route for approving entry through pass
-router.post('/approve',ApproveEntry);
-
-module.exports = router;
--- backend/routes/passverify.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { CheckEntry } = require('../controllers/passverificationcontroller');
-
-
-// Protected route for verifying a pass
-router.get('/verify',CheckEntry);
-
-module.exports = router;
