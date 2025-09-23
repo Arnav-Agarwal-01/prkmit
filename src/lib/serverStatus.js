3420135010f815970f643d0ffc3e6b2c42f5685d
Arnav Agarwal <137907741+Arnav-Agarwal-01@users.noreply.github.com>
2025-09-23 19:01:38 +0530

    server down for 4th yrs

--- src/lib/serverStatus.js
+++ src/lib/serverStatus.js
@@ -1,3 +1,3 @@
 // Simple server status - change this to control redirects
 export const SERVER_IS_UP = false;   // Set to false to redirect to maintenance page
-// change here
+// change here for server down or up
