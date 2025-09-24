6d0c6bdb7122b60c54c452b3cbf928fb7c96a1c4
Arnav Agarwal <137907741+Arnav-Agarwal-01@users.noreply.github.com>
2025-09-24 13:10:49 +0530

    server up

--- src/lib/serverStatus.js
+++ src/lib/serverStatus.js
@@ -1,3 +1,3 @@
 // Simple server status - change this to control redirects
-export const SERVER_IS_UP = false;   // Set to false to redirect to maintenance page
+export const SERVER_IS_UP = true;   // Set to false to redirect to maintenance page
 // change here for server down or up.     
